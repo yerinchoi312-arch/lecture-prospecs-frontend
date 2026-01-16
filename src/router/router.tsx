743a1e0e0a9e06203ffc5565e44c607b126c3eb3
yerin <sky970312@naver.com>
2026-01-16 12:07:31 +0900

    router 입장 권한 제어

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,8 +1,21 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, redirect } from "react-router";
 import Layout from "../layouts/Layout.tsx";
 import Home from "../pages/Home.tsx";
 import Register from "../pages/Register.tsx";
 import Login from "../pages/Login.tsx";
+import useAuthStore from "../store/useAuthStore.ts";
+
+//loader : 해당 주소에 사용자가 가려고 할 때 ( 요청), 화면에 출력해주기 이전 실행되는 함수
+//         조건을 걸고, 아무런 문제가 없으면 null을 반환해서 화면이 출력이 되도록 해야함
+const guestOnlyLoader = () => {
+    const isLoggedIn = useAuthStore.getState().isLoggedIn;
+    if(isLoggedIn) {
+        //navigate 와 redirect 차이
+        //navigate를 쓰려면 useNavigate -> use. 컴포넌트에 안에서 보낼 때
+        return redirect("/")
+    }
+    return null;
+}
 
 export const router = createBrowserRouter([
     {
@@ -10,8 +23,8 @@ export const router = createBrowserRouter([
         element : <Layout />,
         children:[
             {index:true, element:<Home/>},
-            {path:"login", element:<Login/>},
-            {path:"register", element:<Register/>},
+            {path:"login", element:<Login/>,loader:guestOnlyLoader},
+            {path:"register", element:<Register/>,loader:guestOnlyLoader},
         ]
     }
 ])
